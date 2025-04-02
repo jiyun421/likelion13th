a3f8cbca0596e5acd3bc1bb49ac6d466bf53f474
jiyun421 <ju4480@naver.com>
2025-04-02 23:20:56 +0900

    5주차 django 심화 과제 완료

---
+++ seminar_project/lionapp/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('create/',views.create_post),
+    path('get/<int:pk>/',views.get_post), 
+    path('all/',views.get_posts_all),
+    path('update/<int:pk>/',views.update_post), 
+    path('delete/<int:pk>/',views.delete_post)
+]
